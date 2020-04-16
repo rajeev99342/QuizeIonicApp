fe4019442dabbd2a38db1c059f45cc3e2c8d2a90
nrbhushan <rashtrabhushan2@gmail.com>
2020-04-16 19:45:01 +0530

    Create-Group Work

--- src/app/home/create-group/create-group.page.ts
+++ src/app/home/create-group/create-group.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams, PopoverController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-create-group',
   templateUrl: './create-group.page.html',
@@ -9,16 +11,48 @@ import { Storage } from '@ionic/storage';
 export class CreateGroupPage implements OnInit {
   abc: any = this.navParams.get('alarmClosureRequest');
   user_name: string;
-  constructor(private pop : PopoverController,public navParams: NavParams, private storage: Storage) { }
+  baserURL: string = "http://127.0.0.1:9090/RestApi/kidder/main";
+  groupForm: FormGroup;
+  grp_name: FormControl;
+  grp_desc: FormControl;
+  constructor(private pop : PopoverController,public navParams: NavParams, private storage: Storage, private http: HttpClient) { }
 
   ngOnInit() {
     this.storage.get('kidder_user').then((username)=>{
       this.user_name = username;
     });
+    this.createGrpFormControls();
+    this.createGrpForm();
+  }
+  createGrpFormControls()
+  {
+        
+    this.grp_name = new FormControl("", [
+      Validators.required
+    ]);
+    this.grp_desc = new FormControl("", [
+      Validators.required
+    ]);
+  }
+
+  createGrpForm()
+  {
+    this.groupForm = new FormGroup({
+      grp_name: this.grp_name,
+      grp_desc: this.grp_desc
+    });
   }
 
   async createGroup() {
     this.abc = null;
+    const jsonData = JSON.stringify({
+      grp_admin: this.user_name,
+      grp_name: this.groupForm.value.grp_name,
+      grp_desc: this.groupForm.value.grp_desc,
+    });
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json'});  
+    (this.http.post(this.baserURL+"/saveGroupData",jsonData,{responseType: 'json', headers})).subscribe((result)=>{
+    });
     await this.pop.dismiss(true);
   }
 
